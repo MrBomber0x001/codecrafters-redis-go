343a7d8800ab01980797bf80669e0a5152896880
Yousef Meska <mrbomberboy123@gmail.com>
2023-03-23 03:28:52 +0200

    added the PONG functionality

--- app/server.go
+++ app/server.go
@@ -20,9 +20,20 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	defer conn.Close()
+
+	buff := make([]byte, 1024)
+
+	if _, err := conn.Read(buff); err != nil {
+		fmt.Println("erro reading from client", err.Error())
+		os.Exit(1)
+	}
+
+	conn.Write([]byte("+PONG\r\n"))
 }
