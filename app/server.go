c77f65f8be01d60762db81d79ecee54428064195
Yousef Meska <mrbomberboy123@gmail.com>
2023-03-25 04:58:14 +0200

    added concurrent clients handler

--- app/server.go
+++ app/server.go
@@ -4,28 +4,36 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"sync"
 	// Uncomment this block to pass the first stage
 	// "net"
 	// "os"
 )
 
-func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
+var (
+	wg sync.WaitGroup
+)
 
-	// Uncomment this block to pass the first stage
+func main() {
+	wg.Add(1)
+	fmt.Println("Logs from your program will appear here!")
 
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go webWorker(conn)
 	}
+}
 
+func webWorker(conn net.Conn) {
 	defer conn.Close()
 
 	buff := make([]byte, 1024)
@@ -37,5 +45,4 @@ func main() {
 		}
 		conn.Write([]byte("+PONG\r\n"))
 	}
-
 }
